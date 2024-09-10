a715fdf0e0dc07cf27b6d71e377be398a6e5668f
Cameron Hyde <chyde@neoformit.com>
2024-09-11 06:58:34 +1000

    Use DB cache

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -109,6 +109,12 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'django_cache',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
