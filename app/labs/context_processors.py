2e4e99a2df42c155783049aafe38f19ddd42164f
Cameron Hyde <chyde@neoformit.com>
2024-09-29 09:03:24 +1000

    Lab content URLs defined in settings

--- app/labs/context_processors.py
+++ app/labs/context_processors.py
@@ -6,8 +6,5 @@ from django.conf import settings as app_settings
 def settings(request):
     """Include some settings variables."""
     return {
-        'title': None,  # prevents variable not found templating error
         'HOSTNAME': app_settings.HOSTNAME,
-        "GITHUB_CONTENT_ROOT_BASE_URL":
-            app_settings.GITHUB_CONTENT_ROOT_BASE_URL,
     }
