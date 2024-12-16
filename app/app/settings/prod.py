271892e91709b3e503e893144bcf87f7628819aa
Cameron Hyde <chyde@neoformit.com>
2024-12-16 12:46:59 +1000

    Fix ManifestStaticFilesStorage

--- app/app/settings/prod.py
+++ app/app/settings/prod.py
@@ -28,8 +28,15 @@ if ADMIN_EMAIL:
     ]
 
 # Use manifest to manage static file versions for cache busting:
-STATICFILES_STORAGE = ('django.contrib.staticfiles.storage'
-                       '.ManifestStaticFilesStorage')
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": ('django.contrib.staticfiles.storage'
+                       '.ManifestStaticFilesStorage'),0
+    },
+}
 
 SENTRY_DNS = os.getenv('SENTRY_DNS')
 if SENTRY_DNS:
