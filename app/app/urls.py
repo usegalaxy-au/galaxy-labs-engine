fc305108849c0512ce201d3591972bc064e98204
Cameron Hyde <chyde@neoformit.com>
2025-01-04 13:25:31 +1000

    Explicitly add static to urlpatterns

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,11 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('labs.urls')),
 ]
+
+if settings.CLI_DEV:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT,
+    )
 
 handler400 = 'labs.views.custom_400'
