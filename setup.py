c0fee0cf35c2611f03cfb2fd5fbce13d4e3eea62
Cameron Hyde <chyde@neoformit.com>
2025-02-20 05:07:16 +1000

    Template lab_name in <span> for easier manipulation

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
+from pathlib import Path
 from setuptools import setup, find_packages
+
+version = (Path(__file__).parent / "VERSION").read_text().strip()
 
 setup(
     name="galaxy-labs-engine",
-    version="1.0.0",
+    version=version,
     packages=find_packages(where='app'),
     package_dir={'': 'app'},
     include_package_data=True,
