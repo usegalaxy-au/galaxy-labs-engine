6fe66588854d2599e93c5d5bfcfa5dff8ef5f1ba
Cameron Hyde <chyde@neoformit.com>
2025-11-26 08:31:20 +1000

    Ignore "unique constraint failed" race condition in lab cache

--- app/labs_engine/labs/cache.py
+++ app/labs_engine/labs/cache.py
@@ -21,12 +21,12 @@ CACHE_KEY_IGNORE_GET_PARAMS = (
     'cache',
     'nonce',
 )
-CACHE_IGNORE_ERRORS = (
-    'NOT NULL constraint failed',
-    'UNIQUE constraint failed',
-)
 NOCACHE = settings.NOCACHE
 NO_WEB_CACHE = os.getenv('NO_WEB_CACHE', False)  # Development only
+IGNORE_ERRORS = (
+    'UNIQUE constraint failed',
+    'NOT NULL constraint failed',
+)
 
 logger = logging.getLogger('django.cache')
 
@@ -103,9 +103,10 @@ class LabCache:
             try:
                 lab.save()
             except IntegrityError as exc:
-                for err_phrase in CACHE_IGNORE_ERRORS:
-                    if err_phrase in str(exc):
-                        logger.warning('IntegrityError: ' + str(exc))
+                for phrase in IGNORE_ERRORS:
+                    if phrase in str(exc):
+                        logger.warning(
+                            f'Ignoring error updating CachedLab: {exc}')
                         return None
                 raise exc
         else:
@@ -119,9 +120,10 @@ class LabCache:
                 )
                 lab.save()
             except IntegrityError as exc:
-                for err_phrase in CACHE_IGNORE_ERRORS:
-                    if err_phrase in str(exc):
-                        logger.warning('IntegrityError: ' + str(exc))
+                for phrase in IGNORE_ERRORS:
+                    if phrase in str(exc):
+                        logger.warning(
+                            f'Ignoring error updating CachedLab: {exc}')
                         return None
                 raise exc
             logger.debug(f"Created new CachedLab for key {cache_key} - {url}")
