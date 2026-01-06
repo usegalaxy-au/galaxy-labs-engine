7d4ab88bd3ed9b8e30fb436c8e421e50a323e01f
Cameron Hyde <chyde@neoformit.com>
2026-01-06 10:10:12 +1000

    Basic dashboard working

--- app/labs_engine/reporting/urls.py
+++ app/labs_engine/reporting/urls.py
@@ -2,8 +2,10 @@
 
 from django.urls import path
 
-from . import api
+from . import api, views
 
 urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('api/usage', views.get_usage_data, name='usage_data'),
     path('api/logs/upload', api.upload_logs, name='upload_logs'),
 ]
